df0cec62151e0cbb96ef83d57d8ed5200641bdee
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-07 11:04:38 +0530

    new codes : master

--- CodeForces Tier01/omkar.cpp
+++ CodeForces Tier01/omkar.cpp
@@ -10,39 +10,6 @@ int main()
     cin >> m;
     while (m--)
     {
-        int i;
-        cin >> i;
-        ll a[i];
-
-        for (ll i = 0; i < i; i++)
-            cin >> a[i];
-        ll res = 0;
-
-        for (ll i = 0; i < i - 1; i++)
-        {
-            if (a[i] > a[i + 1])
-            {
-                res += abs(a[i] - a[i + 1]);
-            }
-        }
-
-        cout << res << "\n";
-    }
-    return 0;
-}
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int m;
-    cin >> m;
-    while (m--)
-    {
         int n;
         cin >> n;
         ll a[n];
