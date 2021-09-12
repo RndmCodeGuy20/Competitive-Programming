f47a4c6fb1561dc4feaa8dfd4fac274a681b9b6b
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-12 09:03:09 +0530

    test failed log : master

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
+
+    cout << "Helllo " << endl;
     // cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
